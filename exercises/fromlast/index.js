0be730a18a3727f61528171dda5fd5e61241e4c1
Xiaomei Wang <xiaomei.wang07@gmail.com>
2019-10-25 14:34:04 +0800

    tree

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,25 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+	// 2 Pointers, Slow and Fast.
+	let slow, fast;
+	slow = fast = list.getFirst();
+
+	// Point Fast n steps from head.
+	while(n !== 0) {
+		fast = fast.next;
+		n--;
+	}
+
+	// Advance both Slow and Fast, 1 by 1, until Fast reaches the last node.
+	while(fast.next) {
+		slow = slow.next;
+		fast = fast.next;
+	}
+
+	// The Slow is the answer.
+	return slow;
+}
 
 module.exports = fromLast;
